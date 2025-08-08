05f0ef1d6abeb371042e1d166fad964f557f682b
qpinme <qpinme@gmail.com>
2025-08-08 10:39:55 +0530

    Remove tooltip usage to fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
